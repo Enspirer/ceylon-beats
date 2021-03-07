8b4a65ab5765059df82995df8b4560715c9fe8af
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-07 12:02:54 +0530

    License Details Pages

--- resources/views/backend/license/includes/licese_details_page.blade.php
+++ resources/views/backend/license/includes/licese_details_page.blade.php
@@ -1,4 +1,27 @@
 <div class="form-group">
-    <label>Add Creator</label>
-    <input type="text" class="form-control" name="author_name">
+    <label>License Name</label>
+    <input type="text" class="form-control" name="license_name">
+</div>
+
+<div class="form-group">
+    <label>License Agreement</label>
+    <textarea type="text" class="form-control" name="license_agreement" rows="10"></textarea>
+</div>
+
+<div class="form-group">
+    <label>Description</label>
+    <textarea type="text" class="form-control" name="description" rows="10"></textarea>
+</div>
+
+<div class="form-group">
+    <label>Status</label>
+    <select type="text" class="form-control" name="status">
+        <option value="1">Enabled</option>
+        <option value="0">Disabled</option>
+    </select>
+</div>
+
+<div class="form-group">
+    <label>License Type</label>
+    <input type="text" class="form-control" name="license_type">
 </div>
