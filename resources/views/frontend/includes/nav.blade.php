d2e8408a919559f79428f64299bd91a5beac43a8
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-08 12:37:23 +0530

    Navbar Blades

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -5,9 +5,6 @@
                 <img src="{{url('/ceylon_beats_theme/assets/image/Image 15@2x.png')}}" alt="" srcset="" />
                 <select class="form-select" aria-label="Default select example">
                     <option selected>LKR</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
                 </select>
             </div>
         </div>
