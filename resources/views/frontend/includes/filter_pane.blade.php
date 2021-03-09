94502e049024ec43ad7301a5b4a5198d7fb3c7ad
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-09 19:37:39 +0530

    Filter Pane

--- resources/views/frontend/includes/filter_pane.blade.php
+++ resources/views/frontend/includes/filter_pane.blade.php
@@ -55,16 +55,15 @@
                             <div class="row d-flex justify-content-between">
                                 <a href="{{route('frontend.explorer',[$genre->name,'null','null','null','null'])}}">
                                     <div class="row form-check">
-                                        <label class="cbx" for="cbx">
-                                            <div class="flip">
-                                                <div class="front"></div>
-                                                <div class="back">
-                                                    <svg width="15" height="17" viewBox="1 0 15 29">
-                                                        <path d="M2 8.5L6 12.5L14 1.5"></path>
-                                                    </svg>
-                                                </div>
+                                        <div class="flip">
+                                            <div class="front"></div>
+                                            <div class="back">
+                                                <svg width="15" height="17" viewBox="1 0 15 29">
+                                                    <path d="M2 8.5L6 12.5L14 1.5"></path>
+                                                </svg>
                                             </div>
-                                        </label>
+                                        </div>
+
                                         <p class="form-check-label" for="exampleCheck1">{{$genre->name}}</p>
                                     </div>
                                 </a>
