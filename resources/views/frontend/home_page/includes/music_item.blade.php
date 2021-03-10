b805c00a9193fd17f278fe47bf921ea362283079
kavindutheekshana@gmail.com <Kavindu1997>
2021-03-10 12:36:12 +0530

    Add To Cart Button And Favorite Button

--- resources/views/frontend/home_page/includes/music_item.blade.php
+++ resources/views/frontend/home_page/includes/music_item.blade.php
@@ -17,11 +17,8 @@
         </a>
 
         <div class="row">
-            <i>
-                <svg xmlns="http://www.w3.org/2000/svg" width="19.619" height="18" viewBox="0 0 19.619 18">
-                    <path id="Icon_metro-favorite" data-name="Icon metro-favorite" d="M14.951,24.427l-1.422-1.29c-5.052-4.586-8.387-7.612-8.387-11.315a5.337,5.337,0,0,1,5.4-5.4,5.887,5.887,0,0,1,4.414,2.045,5.887,5.887,0,0,1,4.414-2.045,5.337,5.337,0,0,1,5.4,5.4c0,3.7-3.335,6.729-8.387,11.315Z" transform="translate(-5.141 -6.427)" fill="#8f8f8f"></path>
-                </svg>
-            </i>
+            <a href="#"> <i class="fa fa-cart-plus" aria-hidden="true"></i></a>
+            <a href="#"> <i class="fa fa-heart" aria-hidden="true"></i></a>
         </div>
         <h3>{{$feature_musirc->music_name}}</h3>
         <div class="type-time">
