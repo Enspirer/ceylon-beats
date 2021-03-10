56fc90d7037f828445c97ff014790e635ebacf3c
kavindutheekshana@gmail.com <Kavindu1997>
2021-03-10 11:44:38 +0530

    Nav Include Contact

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -3,6 +3,7 @@
 @section('title', app_name() . ' | ' . __('labels.frontend.contact.box_title'))
 
 @section('content')
+    @include('frontend.includes.nav')
 @include('frontend.contact_page.map')
 @include('frontend.contact_page.form')
 @endsection
