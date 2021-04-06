6ec3104b37b0db0374e3ba1f6056fba4d61b51ce
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-06 21:08:07 +0530

    Setting Seeder

--- database/seeds/SettingsSeeder.php
+++ database/seeds/SettingsSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use DB;
 class SettingsSeeder extends Seeder
 {
     /**
@@ -11,6 +11,20 @@ class SettingsSeeder extends Seeder
      */
     public function run()
     {
-        //
+       DB::table('settings')->insert(
+           [
+               'key' => 'currency',
+               'value' => 'USD'
+           ],[
+               'key' => 'app_title',
+               'value' => 'Ceylon Beats'
+           ],[
+               'key' => 'payment_getway_oparation',
+               'value' => 'sandbox'
+           ],[
+               'key' => 'search_enabled',
+               'value' => '1'
+           ]
+       );
     }
 }
---
+++ resources/views/backend/settings/includes/general_settings.blade.php
@@ -0,0 +1,25 @@
+<div class="card-body">
+    <div class="form-group">
+        <label class="form-label">Currency</label>
+        <select class="form-control">
+            <option value="USD">USD</option>
+            <option value="LKR">LKR</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" value="Nelle Maxwell">
+    </div>
+    <div class="form-group">
+        <label class="form-label">E-mail</label>
+        <input type="text" class="form-control mb-1" value="nmaxwell@mail.com">
+        <div class="alert alert-warning mt-3">
+            Your email is not confirmed. Please check your inbox.<br>
+            <a href="javascript:void(0)">Resend confirmation</a>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="form-label">Company</label>
+        <input type="text" class="form-control" value="Company Ltd.">
+    </div>
+</div>
