cb9a603fd920a11c2031793aa42e6959f1cc65f6
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-07 17:27:34 +0530

    MyCart Function

--- resources/views/frontend/includes/homepage_last_music_item_cart_dialog.blade.php
+++ resources/views/frontend/includes/homepage_last_music_item_cart_dialog.blade.php
@@ -1,9 +1,13 @@
-
+<audio id="myAudioq{{$lastestMusic->id}}">
+    <source src="{{url('files/preview_files/'.$lastestMusic->preview_link)}}" type="audio/ogg">
+    <source src="{{url('files/preview_files/'.$lastestMusic->preview_link)}}" type="audio/mpeg">
+    Your browser does not support the audio element.
+</audio>
 
 <div class="player-margin col-lg-4 col-md-6 col-12 animscroll-init" data-animscroll="fade-up">
     <div class="player">
         <div class="play-button">
-            <a onclick="playAudio('myAudio{{$lastestMusic->id}}','play_iconse{{$lastestMusic->id}}')">
+            <a onclick="playAudio('myAudioq{{$lastestMusic->id}}','play_iconse{{$lastestMusic->id}}')">
                 <div class="play-icon">
                     <i id="play_iconse{{$lastestMusic->id}}" class="fa fa-play" style="font-size: 54px;color: white;"></i>
                 </div>
--- resources/views/frontend/includes/homepage_treanding_item_cart_dialog.blade.php
+++ resources/views/frontend/includes/homepage_treanding_item_cart_dialog.blade.php
@@ -62,5 +62,69 @@
     </form>
 @endforeach
 
+@foreach($latest_music as $lastestMusicL)
+    {{------------------ Modal ------------------ --}}
+    <form action="{{route('frontend.cart.add')}}" method="post">
+        <div class="modal fade" id="exampleModalCenterTM{{$lastestMusicL->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" style="display: none;">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                {{csrf_field()}}
+                <div class="modal-content">
+                    <h1>Select Your Licenses</h1>
+                    <select class="form-control" name="license_name" id="licesensect{{$lastestMusicL->id}}" onchange="selectPackage('{{$lastestMusicL->id}}','licesensect{{$lastestMusicL->id}}','inpack_price{{$lastestMusicL->id}}')">
+                        <option selected>Select License</option>
+                        @foreach(json_decode($lastestMusicL->price) as  $priceranger)
+                            <option value="{{$priceranger->license_name}}">{{$priceranger->license_name}}</option>
+                        @endforeach
+                    </select>
+
+                    <input type="hidden" name="music_item_id" value="{{$lastestMusicL->id}}">
+                    <div class="modal-body">
+                        <input type="hidden" name="price_details" value="" id="pricedetails{{$lastestMusicL->id}}">
+                        <input type="hidden" name="music_item_id" value="{{$lastestMusicL->id}}">
+                        <input type="hidden" name="music_name" value="{{$lastestMusicL->music_name}}">
+                        <input type="hidden" name="preview_link" value="{{$lastestMusicL->preview_link}}">
+                        <input type="hidden" name="author_name" value="{{$lastestMusicL->author_name}}">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="play-button">
+                                        <i class="fa fa-play" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <h6>{{$lastestMusicL->music_name}}</h6>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div style="font-size: 19px;font-weight: 700;color: wheat;">
+                                                {{settings('currency')}} <span id="inpack_price{{$lastestMusicL->id}}">0</span>.00
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <h5>{{$lastestMusicL->author_name}}</h5>
+                                        </div>
+                                        <div class="col-md-6">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#" class="btn-cancel-modal" data-dismiss="modal">Cancel</a>
+                        <button type="submit" id="submit_cores{{$lastestMusicL->id}}"   class="btn-cart-modal" disabled>Add to Cart</button>
+                        <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+@endforeach
+
 
 
