b69a5f43726f0da9f2a33b857601888c92bfa7b0
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-12 12:01:29 +0530

    Left Filter Pane

--- resources/views/frontend/includes/filter_pane.blade.php
+++ resources/views/frontend/includes/filter_pane.blade.php
@@ -1,4 +1,4 @@
-<div class="left-filter">
+<div class="left-filter" style="position: relative;height: 630px;">
     <div class="row-filter-top row d-flex justify-content-between">
 
             <svg xmlns="http://www.w3.org/2000/svg" width="22.5" height="15" viewBox="0 0 22.5 15">
