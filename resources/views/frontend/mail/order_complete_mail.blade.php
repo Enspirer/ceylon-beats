99ab8cd0372bcb6213d47261d8ef947adc09054c
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-12 16:26:02 +0530

    Order Complete Mail

--- resources/views/frontend/mail/order_complete_mail.blade.php
+++ resources/views/frontend/mail/order_complete_mail.blade.php
@@ -102,7 +102,7 @@
                                 <tr>
                                     <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
                                         <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">
-                                            <p>Successful payment of USD {{number_format($invoice_details->total,2)}} for order reference #{{$invoice_details->id}}.<br> Please log in to <a href="http://ceylonbeatz.com/">www.ceylonbeatz.com </a> to.</p>
+                                            <p style="font-size: 15px;">Successful payment of USD {{number_format($invoice_details->total,2)}} for order reference #{{$invoice_details->id}}.<br> Please log in to <a href="http://ceylonbeatz.com/">www.ceylonbeatz.com </a> to.</p>
                                         </span>
 
                                         <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
@@ -113,6 +113,7 @@
                                                         <tbody>
                                                         <tr>
                                                             <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;">
+                                                                <br>
                                                                 <a href="http://ceylonbeatz.com" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">Visit</a> </td>
                                                         </tr>
                                                         </tbody>
