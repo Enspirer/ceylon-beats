1aec9a98baeb7e105a421caf0b3ff97015364636
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-12 14:21:47 +0530

    Order Complete Mail Modification

--- resources/views/frontend/mail/order_complete_mail.blade.php
+++ resources/views/frontend/mail/order_complete_mail.blade.php
@@ -1,1 +1,7 @@
-Successful payment of USD {{$invoice_details->total}} for order reference #{{$invoice_details->id}}.Now you can download your purchased music
+<h2>Your Order Successful</h2>
+<br>
+<p>Successful payment of USD {{$invoice_details->total}} for order reference #{{$invoice_details->id}}.<br>
+    Please log in to <a href="http://ceylonbeatz.com/">www.ceylonbeatz.com </a> to download your music.</p>
+
+<p><b>Please contact us for any support you may need. info@ceylonbeatz.com</b></p>
+Add ( email)
